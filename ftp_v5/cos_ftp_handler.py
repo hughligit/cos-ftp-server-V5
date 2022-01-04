faf478dad73c2b80baee05e8ed81a3d5aee91ff2
hughli <hughli@tencent.com>
2022-01-04 19:24:16 +0800

    fix: compatible with FTP MLST command

--- ftp_v5/cos_ftp_handler.py
+++ ftp_v5/cos_ftp_handler.py
@@ -6,3 +6,6 @@ from pyftpdlib.handlers import FTPHandler
 class CosFtpHandler(FTPHandler):
     def __init__(self, *args, **kwargs):
         FTPHandler.__init__(self, *args, **kwargs)
+
+    def ftp_MLST(self, path):
+        return self.ftp_LIST(path)
